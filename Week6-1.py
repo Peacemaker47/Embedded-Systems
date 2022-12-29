ba7aa54514fa78c6ebf60ef7023460c56e8b3861
Peacemaker47 <zubaier.2000@gmail.com>
2022-12-29 22:51:24 +0800

    Deleted extra files

--- Week6-1.py
+++
@@ -1,8 +0,0 @@
-from machine import Pin, Timer
-led = Pin(25, Pin.OUT)
-
-led.value(0)
-
-timer=Timer(-1)
-
-timer.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:led.value(not led.value()))
